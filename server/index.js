37155b8937a27d82c5be9fadf4d45dc55a59eba3
cycjimmy <cycjimmy@gmail.com>
2017-11-08 10:15:15 +0800

    add weixinjssdk demo

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const
 
 let
   app = express()
+  , wxjssdk = require('weixinjssdk')
   , port = process.env.PORT || 3000
 ;
 
@@ -12,8 +13,21 @@ let
 app.use('/static', express.static(path.resolve('static')));
 
 // set api
-app.get('/api', (req, res) => {
-  res.send('mock api');
+app.get('/api/wxJssdk', (req, res) => {
+  let
+    _getUrl = req => req.protocol + '://' + req.headers.host + req.originalUrl.split('#')[0]
+  ;
+
+  wxjssdk({
+    appid: 'wxcc6445076f2002c3',
+    secret: 'd4624c36b6795d1d99dcf0547af5443d',
+    url: _getUrl(req)
+  })
+    .then(data => {
+      res.send(data);
+    }, (err) => {
+      res.send(err);
+    });
 });
 
 app.listen(port, () => {
