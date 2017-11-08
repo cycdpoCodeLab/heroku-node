304cb45ff9571c6dc93227fb2ee0f7c7214d01fe
cycjimmy <cycjimmy@gmail.com>
2017-11-08 13:06:45 +0800

    add wxVerify

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const
   express = require('express')
+  , crypto = require('crypto')
   , path = require('path')
 ;
 
@@ -8,6 +9,53 @@ let
   , wxjssdk = require('weixinjssdk')
   , port = process.env.PORT || 3000
 ;
+
+const token = 'cycjimmy';
+
+let handleWechatRequst = (req, res, next) => {
+  let
+    {signature, timestamp, nonce, echostr} = req.query
+  ;
+
+  if (!signature || !timestamp || !nonce) {
+    return res.send('invalid request');
+  }
+
+  if (req.method === 'POST') {
+    console.log('handleWechatRequst.post:', {body: req.body, query: req.query});
+  }
+
+  if (req.method === 'GET') {
+    console.log('handleWechatRequst.get:', {body: req.body});
+    if (!echostr) {
+      return res.send('invalid request');
+    }
+  }
+
+  let params = [token, timestamp, nonce];
+  params.sort();
+  let hash = crypto.createHash('sha1');
+  let sign = hash.update(params.join('')).digest('hex');
+
+  if (signature !== sign) {
+    res.send('invaid sign');
+  }
+
+  if ('GET' === req.method) {
+    res.send(echostr ? echostr : 'invaid sign');
+  } else {
+    let postdata='';
+    req.addListener("data",(postchunk)=>{
+      postdata += postchunk;
+    });
+
+    req.addListener("end",function(){
+      console.log(postdata);
+      res.send('success');
+    });
+  }
+};
+
 
 // static
 app.use('/static', express.static(path.resolve('static')));
@@ -33,6 +81,10 @@ app.get('/api/wxJssdk', (req, res) => {
       res.send(err);
     });
 });
+
+// wxVerify
+app.get('/api/wxVerify', handleWechatRequst);
+app.post('/api/wxVerify', handleWechatRequst);
 
 app.listen(port, () => {
   console.log('App is listening at port ' + port + '!');
